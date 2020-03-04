95379083b7f605d813b77f19357260bff7556d39
tonibardina7 <tonibardina@gmail.com>
2020-03-04 11:46:49 +0100

    feature/Changing repo structure and adding TTF model to mmdetection

---
+++ models/get_models.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget "https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/models/atss/atss_r50_fpn_1x_20200113-a7aa251e.pth" -P ./models/model_zoo     
+wget "https://open-mmlab.s3.ap-northeast-2.amazonaws.com/mmdetection/models/gcnet/mask_rcnn_r4_gcb_c3-c5_r50_fpn_1x_20190602-18ae2dfd.pth" -P ./models/model_zoo
+wget "http://downloads.zjulearning.org.cn/ttfnet/ttfnet18_1x-fe6884.pth" -P ./models/model_zoo
