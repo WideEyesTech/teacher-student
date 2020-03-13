24ea1b7102359ff0ef861dc85b45147685f0f3d2
Toni Bardina Comas <toni@tonibardina.tonibardina>
2020-03-13 15:59:36 +0000

    bugfix/fix problem with sampler on extending flags list

--- models/ttfnet/mmdet/datasets/loader/build_loader.py
+++ models/ttfnet/mmdet/datasets/loader/build_loader.py
@@ -5,7 +5,7 @@ from mmcv.parallel import collate
 from mmcv.runner import get_dist_info
 from torch.utils.data import DataLoader
 
-from .sampler import DistributedGroupSampler, DistributedSampler, GroupSampler, WeightedWeakLabelsGroupSampler
+from .sampler import DistributedGroupSampler, DistributedSampler, GroupSampler, CustomSampler
 
 if platform.system() != 'Windows':
     # https://github.com/pytorch/pytorch/issues/973
@@ -24,7 +24,7 @@ def build_dataloader(dataset,
     if dist:
         rank, world_size = get_dist_info()
         if shuffle:
-            sampler = DistributedGroupSampler(dataset, imgs_per_gpu,
+            sampler = CustomSampler(dataset, imgs_per_gpu,
                                                world_size, rank)
         else:
             sampler = DistributedSampler(
